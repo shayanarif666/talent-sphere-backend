02d0dbbf86d8a108efae841454a51f6f92bc68a1
Shayan Ali <spark.techelix@outlook.com>
2026-03-02 23:32:43 +0500

    Global Error Middleware Print

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ app.get("/", (req, res) => {
 });
 app.use("/api/auth", authRoutes);
 
+app.use((err, req, res, next) => {
+  console.error("ERROR", err);
+
+  return res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    errors: err.errors || [],
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
+});
+
 
 // Server listen
 app.listen(process.env.PORT, () => {
