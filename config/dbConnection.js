e6aa46e83266868e38d44faa4dd8f527385ebaad
Shayan Ali <spark.techelix@outlook.com>
2026-03-03 16:15:09 +0500

    MONGO DB URL DIRECT ACCESS

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,44 +1,16 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const dbConnection = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL);
-//     console.log("Mongo Db Connection successfully");
-//   } catch (error) {
-//     console.log("error mongo db connection == >", error);
-//   }
-// };
-
-// module.exports = {
-//   dbConnection
-// };
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://talentsphere26_db_user:HJpqsM83aWn2vEig@talentspheredb.emmhk1o.mongodb.net/?appName=talentsphereDB";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function dbConnection() {
+const dbConnection = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    await mongoose.connect("mongodb+srv://talentsphere26_db_user:HJpqsM83aWn2vEig@talentspheredb.emmhk1o.mongodb.net/?appName=talentsphereDB");
+    console.log("Mongo Db Connection successfully");
+  } catch (error) {
+    console.log("error mongo db connection == >", error);
   }
-}
+};
 
 module.exports = {
   dbConnection
-}
+};
+
 
