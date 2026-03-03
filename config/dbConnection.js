a393d1b65db2741b2717700f6cb2973b9339576b
Shayan Ali <spark.techelix@outlook.com>
2026-03-03 16:18:32 +0500

    Render deployment issue resolved

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect("mongodb+srv://talentsphere26_db_user:HJpqsM83aWn2vEig@talentspheredb.emmhk1o.mongodb.net/?appName=talentsphereDB");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Mongo Db Connection successfully");
   } catch (error) {
     console.log("error mongo db connection == >", error);
